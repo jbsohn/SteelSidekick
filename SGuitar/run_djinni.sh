186d6c6be24b8ef1c7e197b62ca4f91060b2a1e6
John Sohn <john.sohn@gmail.com>
2018-07-06 20:26:34 -0500

    add DJINNI to project

---
+++ SGuitar/run_djinni.sh
@@ -0,0 +1,37 @@
+#! /usr/bin/env bash
+
+base_dir=$(cd "`dirname "0"`" && pwd)
+cpp_out="$base_dir/generated-src/cpp"
+jni_out="$base_dir/generated-src/jni"
+objc_out="$base_dir/generated-src/objc"
+java_out="$base_dir/generated-src/java/com/steelsidekick/sguitar"
+java_package="com.steelsidekick.sguitar"
+namespace="SG"
+objc_prefix="SG"
+djinni_file="sguitar.djinni"
+
+rm -rf $cpp_out
+rm -rf $jni_out
+rm -rf $objc_out
+rm -rf $java_out
+
+deps/djinni/src/run \
+   --java-out $java_out \
+   --java-package $java_package \
+   --ident-java-field mFooBar \
+   \
+   --cpp-out $cpp_out \
+   --cpp-namespace $namespace \
+   \
+   --jni-out $jni_out \
+   --ident-jni-class NativeFooBar \
+   --ident-jni-file NativeFooBar \
+   \
+   --objc-out $objc_out \
+   --objc-type-prefix $objc_prefix \
+   \
+   --objcpp-out $objc_out \
+   \
+   \
+   --idl $djinni_file
+#    --ident-cpp-enum NativeFooBar \
